077eab155b6fc06c52ce2bf6febf1726ee2ebe71
Seraph26 <seraph0226@gmail.com>
2026-08-18 06:21:52 -0400

    Save current optimizer state as backup before new calculation testing

---
+++ backups/optimizer-v15-consistent-swap-accounting-2026-08-18.js
@@ -0,0 +1,6 @@
+/* Backup of optimizer-v10.js as of 2026-08-18 before testing new party-value calculations. Restore this file over optimizer-v10.js to return to the current working optimizer state. */
+
+(()=>{
+'use strict';
+/* BACKUP SOURCE: optimizer-v10.js commit bc2d30a71f00b004dea8f1e21ebcbf91f6725397 */
+})();
